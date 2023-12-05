f96db8edab5593fee8cd53e90fc4022cc388b59e
Din56 <151639804+Din56@users.noreply.github.com>
2023-12-05 21:11:44 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a + b
+}
+function sub(a, b) {
+    return a - b
+}
+function mui(a, b) {
+    return a * b
+}
+function div(a, b) {
+    return a / b
+}
+function add2(a, b, c) {
+    return a + b + c
+}
+function sub2(a, b, c) {
+    return a - b - c
+}
+function mui2(a, b, c) {
+    return a * b * c
+}
+function div2(a, b, c) {
+    return a / b / c
+}
